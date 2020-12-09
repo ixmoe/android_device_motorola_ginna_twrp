15b2e1d6db15fb544d52754983eb6bed94c98b6a
Beta Soft <betaxab@gmail.com>
2020-12-10 07:29:55 +0800

    ginna: use twrp tzdata

--- device.mk
+++ device.mk
@@ -50,6 +50,10 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
+# tzdata
+PRODUCT_PACKAGES += \
+    tzdata_twrp
+
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
